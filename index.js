fd9a5eb494aceeae23dc1235dcf1e06cab7a8078
Caroline Verticchio <carolineverticchio@Carolines-MacBook-Pro.local>
2019-01-13 15:39:45 -0500

    built image array, added text to images in gallery

--- index.js
+++ index.js
@@ -1,14 +1,69 @@
-const gallery = document.querySelector(".gallery-images");
-const images = ["Get_Fit", "Grand_Canyon", "Power_Walk", "Elliptical_Weight_Loss", "Off_Season_Weight_Loss", "Central_Park",  "5K", "Coastal", "Elliptical", "Incline_Train", "Paris", "Lose_Weight"];
+const images = [
+    {
+        image: 'Get_Fit',
+        title: 'Get Fit with Jillian Michaels',
+        subtitle: 'weight loss'
+    }, {
+        image: 'Grand_Canyon',
+        title: 'Grand Canyon',
+        subtitle: 'incline trainer'
+    }, {
+        image: 'Power_Walk',
+        title: 'Power Walk',
+        subtitle: 'walking'
+    }, {
+        image: 'Elliptical_Weight_Loss',
+        title: 'Elliptical Weight Loss',
+        subtitle: 'weight loss'
+    }, {
+        image: 'Off_Season_Weight_Loss',
+        title: 'Off Season Weight Loss',
+        subtitle: 'cycling'
+    }, {
+        image: 'Central_Park',
+        title: 'Central_Park',
+        subtitle: 'fitness'
+    }, {
+        image: '5K',
+        title: '5K Beginner Training',
+        subtitle: 'running'
+    }, {
+        image: 'Coastal',
+        title: 'Going Costal',
+        subtitle: 'running'
+    }, {
+        image: 'Elliptical',
+        title: 'Elliptical Weight Loss',
+        subtitle: 'weight loss'
+    }, {
+        image: 'Incline_Train',
+        title: 'Incline Train',
+        subtitle: 'incline trainer'
+    }, {
+        image: 'Paris',
+        title: 'Paris',
+        subtitle: 'running'
+    }, {
+        image: 'Lose_Weight',
+        title: 'Lose Weight with Jillian Michaels',
+        subtitle: 'weight loss'
+    }
+]
 
-function generateHTML( image ) {
+const gallery = document.querySelector(".gallery-images");
+
+function generateHTML( { image, title, subtitle } ) {
     return `
-        <div class="program">
-          <img src="assets/images/${image}.png">
-          <div class="program-overlay">
-            <button class="button-view">View →</button>
-          </div>
-        </div>
+        <figure class="program">
+            <img src="assets/images/${image}.png" alt={title}>
+            <figcaption>
+                <div>${title}</div>
+                <div>${subtitle}</div>
+            </figcaption>
+            <div class="program-overlay">
+              <button class="button-view">View →</button>
+            </div>
+        </figure>
     `;
 }
 
